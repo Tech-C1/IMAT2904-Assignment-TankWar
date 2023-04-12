4728994b0d0ecb8daeea940360f1b18a57206a89
JoshElo1 <jte@gmx.co.uk>
2023-04-12 21:48:07 +0100

    Comments
    Comments added to the Finite State Machine Scripts.

--- Assets/Scripts/FSMScripts/BaseState.cs
+++ Assets/Scripts/FSMScripts/BaseState.cs
@@ -3,8 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Class called BaseState
 public abstract class BaseState
 {
+    // Public abstract Types called StateUpdate, StateEnter and StateExit
     public abstract Type StateUpdate();
     public abstract Type StateEnter();
     public abstract Type StateExit();
